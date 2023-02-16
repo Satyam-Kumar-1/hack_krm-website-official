2596644cfeb2bcdfd01e81618ed5c5ac28d8b33c
Satyam-Kumar-1 <satyamkrsatyam1@gmail.com>
2023-02-16 23:03:32 +0530

    Changes to be committed: 	new file:   income & expenses using javascript/about.html 	new file:   income & expenses using javascript/app.js 	new file:   income & expenses using javascript/debug.log 	new file:   income & expenses using javascript/expend.html 	new file:   income & expenses using javascript/html2pdf.bundle.min.js 	new file:   income & expenses using javascript/image/1.jpg 	new file:   income & expenses using javascript/image/2.jpg 	new file:   income & expenses using javascript/image/3.jpg 	new file:   income & expenses using javascript/image/4.jpg 	new file:   income & expenses using javascript/image/wallet2.svg 	new file:   income & expenses using javascript/style.css 	modified:   index.html 	modified:   service.html 	deleted:    sip/script.js 	deleted:    sip/sip.html 	deleted:    sip/style.css 	modified:   sip_cal/script.js 	modified:   sip_cal/sipc.html 	deleted:    sip_cal/sipcalculator.html

--- sip_cal/script.js
+++ sip_cal/script.js
@@ -1,6 +1,26 @@
-// document.getElementById("buttonn").addEventListener("click", myfnc)
-// function myfnc() {
-//   // Grabs the values from input fields
+document.getElementById("buttonn").addEventListener("click", myfnc)
+function myfnc() {
+  // Grabs the values from input fields
+  var p = parseInt(document.getElementById("p").value);
+  var t =parseInt(document.getElementById("t").value);
+  var roi = parseInt(document.getElementById("roi").value);
+  n=12,
+  r=roi/n/100,
+  //result=0,
+  instalments=n*t
+  
+  // selects the result element
+  
+  
+  // var result = document.getElementById("result");
+  // // Returns the result of the percentage portion amount
+  // return (result.textContent = p * Math.pow((1 + r/n), n*t));
+  var res=Math.round(p * Math.pow((1 + r/n), n*t));
+  document.getElementById("result").innerHTML=res;
+};
+// const form = document.querySelector('#tax-form');
+// form.addEventListener('submit', (event)=>{
+//   event.preventDefault();
 //   var p = document.getElementById("p").value;
 //   var t =document.getElementById("t").value;
 //   var roi = document.getElementById("roi").value;
@@ -9,31 +29,9 @@
 //   //result=0,
 //   instalments=n*t
   
-//   // selects the result element
-  
-  
-//   // var result = document.getElementById("result");
-//   // // Returns the result of the percentage portion amount
-//   // return (result.textContent = p * Math.pow((1 + r/n), n*t));
-//   var res=p * Math.pow((1 + r/n), n*t);
-//   document.getElementById("result").innerHTML=res;
-// };
-const form = document.querySelector('#tax-form');
-form.addEventListener('submit', (event)=>{
-  event.preventDefault();
-  var p = document.getElementById("p").value;
-  var t =document.getElementById("t").value;
-  var roi = document.getElementById("roi").value;
-  n=12,
-  r=roi/n/100,
-  //result=0,
-  instalments=n*t
-  
-  var res=Math.round(p * Math.pow((1 + r/n), n*t));
-  return result;
-  // const income = Number(form.income.value);
-  // const tax = calculatorIncomeTax(income);
+//   var res=Math.round(p * Math.pow((1 + r/n), n*t));
+//   // const income = Number(form.income.value);
+//   // const tax = calculatorIncomeTax(income);
 
-  alert(`Expected Return for ${p} is : ${res}`);
-})
-console.log(form());
+//   alert(`Expected Return for ${p} is : ${res}`);
+// })
